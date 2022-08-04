5cf09848f06bbfb6b1d2eb209721babc3a2c653a
ricardocarosa <rickcarosa@yahoo.com>
2022-08-04 17:13:57 -0400

    add user sing in and sign out, private route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -14,14 +17,19 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
-          <Route path='/sign-n' element={<SignIn />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            {/* this is the "outlet" component below */}
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
 
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
